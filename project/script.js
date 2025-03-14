3328d26bc9f9b81daacedb409edabc48c1877ed8
Manuel Auinger <m.auinger@students.htl-leonding.ac.at>
2025-03-14 10:51:22 +0100

    Neue Änderung bie Startseite (erste Animation)

--- project/script.js
+++ project/script.js
@@ -1,1 +1,16 @@
-document.getElementById('box').innerHTML = 'Test'
+document.getElementById('box').innerHTML = ` Auf dieser Website Spielen 2 Spieler gegeneinander Mathe Spiele, um sich zu messen. <br> Viel Spaß <div id = "button" onclick = "getStartButton()">  <img src="./img/icons8-close-50.png" alt=""> </div>`
+
+
+function getStartButton() {
+
+    document.getElementById('box').style.animation = 'buttonForm 1s';
+    setTimeout(document.getElementById('box').style.width = '20%', 1000)
+    document.getElementById('box').style.fontSize = 'larger'
+    document.getElementById('box').innerHTML = `<div onclick = "getCharacter>"> Start Game </div>`
+    
+}
+
+
+function getCharacter() {
+    console.log('test')
+}
