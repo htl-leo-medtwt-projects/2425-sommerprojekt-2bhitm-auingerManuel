317c4947adbca63134f89a6adff823bd2da40b0a
Manuel Auinger <m.auinger@students.htl-leonding.ac.at>
2025-03-20 15:27:33 +0100

    Start der Homepage, und root bei css

--- project/script.js
+++ project/script.js
@@ -1,5 +1,14 @@
 document.getElementById('box').innerHTML = ` Auf dieser Website Spielen 2 Spieler gegeneinander Mathe Spiele, um sich zu messen. <br> Viel Spaß <div id = "button" onclick = "getStartButton()">  <img src="./img/icons8-close-50.png" alt=""> </div>`
-
+let players = [
+     {
+        points: 0,
+        name: ''
+    },
+    {
+        points: 0,
+        name: ''
+    }
+]
 
 function getStartButton() {
     document.getElementById('box').style.animation = 'buttonForm 1s';
@@ -10,20 +19,35 @@ function getStartButton() {
 
 function styling() {
 document.getElementById('box').style.width = '20%'
-document.getElementById('box').style.backgroundColor = '#648DE5';
+document.getElementById('box').style.backgroundColor = '#DDCD8B';
+document.getElementById('box').style.color = '#648DE5';
 document.getElementById('box').style.fontSize = 'larger';
 
 }
 
 function getCharacter() {
     document.getElementById('box').style.display = 'none';
-    document.getElementById('headline').style.display = 'none'
-    document.getElementById('createPlayer').innerHTML = ` <div id="playerOne" class="boxCreate">
+    // document.getElementById('headline').style.display = 'none'
+    document.getElementById('createPlayer').innerHTML = ` 
+        <div id="playerOne" class="boxCreate">
             <label for="pl1">Spieler 1:</label>
             <input type="text" name="pl1" id="pl1">
+
         </div>
         <div id="playerTwo" class="boxCreate">
-            <label for="pl1">Spieler 2:</label>
-            <input type="text" name="pl1" id="pl1">
-        </div>`
+            <label for="pl2">Spieler 2:</label>
+            <input type="text" name="pl2" id="pl2">
+        </div>
+        
+        <div onclick = "homePage()"> Submit </div>
+        `
+}
+
+function homePage() {
+    for(let i = 0; i < 2; i++) {
+        players[i].name = document.getElementById(`pl${i + 1}`).value;
+        
+    }
+    
+    document.getElementById('createPlayer').innerHTML = '';
 }
