52b8fcc62a04a451ad4b009a039ae9bf46c3e1c4
Manuel Auinger <m.auinger@students.htl-leonding.ac.at>
2025-03-21 10:48:19 +0100

    Beginn Homepage

--- project/script.js
+++ project/script.js
@@ -1,3 +1,5 @@
+// Startseite
+
 document.getElementById('box').innerHTML = ` Auf dieser Website Spielen 2 Spieler gegeneinander Mathe Spiele, um sich zu messen. <br> Viel Spaß <div id = "button" onclick = "getStartButton()">  <img src="./img/icons8-close-50.png" alt=""> </div>`
 let players = [
      {
@@ -13,33 +15,31 @@ let players = [
 function getStartButton() {
     document.getElementById('box').style.animation = 'buttonForm 1s';
     setTimeout(styling(), 1000)
-    document.getElementById('box').style.fontSize = 'larger'
-    document.getElementById('box').innerHTML = `<div onclick = "getCharacter()"> Start Game </div>`
+    document.getElementById('box').innerHTML = `<div onclick = "getCharacter()"> <h1> Start Game </h1> </div>`
 }
 
 function styling() {
 document.getElementById('box').style.width = '20%'
-document.getElementById('box').style.backgroundColor = '#DDCD8B';
-document.getElementById('box').style.color = '#648DE5';
 document.getElementById('box').style.fontSize = 'larger';
 
 }
 
 function getCharacter() {
+    document.getElementById('headline').style.display = 'none'
     document.getElementById('box').style.display = 'none';
     // document.getElementById('headline').style.display = 'none'
-    document.getElementById('createPlayer').innerHTML = ` 
+    document.getElementById('createPlayer').innerHTML = `
+        <div> 
         <div id="playerOne" class="boxCreate">
             <label for="pl1">Spieler 1:</label>
             <input type="text" name="pl1" id="pl1">
-
         </div>
         <div id="playerTwo" class="boxCreate">
             <label for="pl2">Spieler 2:</label>
             <input type="text" name="pl2" id="pl2">
         </div>
-        
-        <div onclick = "homePage()"> Submit </div>
+        </div>
+        <div id = "submit" onclick = "homePage()"> Submit </div>
         `
 }
 
@@ -48,6 +48,10 @@ function homePage() {
         players[i].name = document.getElementById(`pl${i + 1}`).value;
         
     }
-    
     document.getElementById('createPlayer').innerHTML = '';
 }
+
+// HomePage
+
+
+
